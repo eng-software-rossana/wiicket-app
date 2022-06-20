a11140bea4c9d06440530578183132118c65b6d1
GabrielMarquesM <raskeng@gmail.com>
2022-06-20 16:01:56 -0300

    Adding buy button to Product Page

--- scenes/product_page/ProductPage.tsx
+++ scenes/product_page/ProductPage.tsx
@@ -1,14 +1,19 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
-import React from 'react';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { useContext } from 'react';
 import { ScrollView, View } from 'react-native';
 import { Card, Paragraph, Title } from 'react-native-paper';
+import Button from '../../components/button/Button';
+import { buttonStyles } from '../../components/button/buttonStyles';
 import { productStyles } from '../../components/product/productStyles';
+import { CartContext, ShoppingList } from '../../context/CartContext';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
 import { cartStyles } from '../cart/cartStyles';
 import { productPageStyles } from './productPageStyles';
 
 const ProductPage = () => {
   const route = useRoute<RouteProp<RootStackParamList, 'ProductPage'>>();
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   const productID = route.params.productID;
   const json_data = {
     productID: '1',
@@ -20,6 +25,10 @@ const ProductPage = () => {
     description:
       'Como todas as GPUs GeForce RTX Series, a GeForce RT 3060 oferece suporte à trifeta das inovações em jogos da GeForce: NVIDIA DLSS, NVIDIA Reflex e NVIDIA Broadcast, que aceleram o desempenho e melhoram a qualidade da imagem. Juntamente com o rastreamento de raios em tempo real, essas tecnologias são a base da plataforma de jogos GeForce, que oferece desempenho e recursos incomparáveis para jogos e jogadores em todos os lugares. Equipado com ventoinhas gêmeas de 90 mm e design prático especial para dissipação de calor, a textura e aparência de carbono exclusivas trazem o máximo de frescor da placa.',
   };
+  const { cartItems, addCartItem, incrementCost } = useContext(
+    CartContext,
+  ) as ShoppingList;
+
   return (
     <View style={productPageStyles.default}>
       <ScrollView>
@@ -31,6 +40,21 @@ const ProductPage = () => {
               style={
                 cartStyles.cartProductPrice
               }>{`R$ ${json_data.cost}`}</Title>
+            <Button
+              style={buttonStyles.buy}
+              text="Comprar"
+              onPress={() => {
+                if (productID) {
+                  addCartItem(productID);
+                  if (!cartItems.includes(productID)) {
+                    incrementCost(json_data.cost);
+                  }
+                  navigation.navigate('TabHome', {
+                    screen: 'Carrinho',
+                  });
+                }
+              }}
+            />
             <Title style={productPageStyles.descriptionLabel}>Descrição:</Title>
             <Paragraph style={productPageStyles.descriptionContent}>
               {json_data.description}
